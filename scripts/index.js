a037a91224e37d6bd3da36a583fb9638147622f1
earielle <demix3991@gmail.com>
2023-12-14 18:38:41 +0700

    feat: complete

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
+const cardsList = document.querySelector(".places__list");
 
-// @todo: DOM узлы
+const removeCard = (card) => {
+  card.remove();
+};
 
-// @todo: Функция создания карточки
+const createCard = (template, cardContent) => {
+  const cardElement = template.querySelector(".places__item").cloneNode(true);
 
-// @todo: Функция удаления карточки
+  cardElement
+    .querySelector(".card__image")
+    .setAttribute("src", cardContent.link);
 
-// @todo: Вывести карточки на страницу
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", () => {
+      removeCard(cardElement);
+    });
+
+  cardElement
+    .querySelector(".card__description")
+    .querySelector(".card__title").textContent = cardContent.name;
+
+  cardsList.append(cardElement);
+};
+
+initialCards.forEach((cardContent) => createCard(cardTemplate, cardContent));
